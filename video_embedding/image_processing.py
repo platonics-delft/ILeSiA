71ceae2c20a13336556e0528c7fea945893816f0
Petr Vanc <petrvancjr@gmail.com>
2025-04-02 14:20:33 +0200

    tfm update

--- video_embedding/image_processing.py
+++ video_embedding/image_processing.py
@@ -5,11 +5,12 @@ import torchvision
 # resize images to 64x64
 saved_img_processing = transforms.Compose(
     [
-        transforms.ToPILImage(),
+        #transforms.ToPILImage(),
         transforms.Resize(
             (64, 64), torchvision.transforms.InterpolationMode.BILINEAR
         ),
         # transforms.Grayscale(num_output_channels=1),
-        transforms.ToTensor(),
+        #transforms.ToTensor(),
+        transforms.Lambda(lambda x: x / 255.0),
     ]
 )
