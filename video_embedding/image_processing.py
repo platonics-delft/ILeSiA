a7142c76ba05a35597705c147a207e2f5a6024d5
Petr Vanc <petrvancjr@gmail.com>
2025-04-02 14:33:59 +0200

    tfm update

--- video_embedding/image_processing.py
+++ video_embedding/image_processing.py
@@ -1,16 +1,18 @@
 
 from torchvision import transforms
 import torchvision
+import torch
 
 # resize images to 64x64
-saved_img_processing = transforms.Compose(
-    [
-        #transforms.ToPILImage(),
-        transforms.Resize(
-            (64, 64), torchvision.transforms.InterpolationMode.BILINEAR
-        ),
-        # transforms.Grayscale(num_output_channels=1),
-        #transforms.ToTensor(),
-        transforms.Lambda(lambda x: x / 255.0),
-    ]
-)
+def saved_img_processing(img):
+    
+    resize_transform = transforms.Compose(
+        [
+            transforms.Resize(
+                (64, 64), torchvision.transforms.InterpolationMode.BILINEAR
+            ),
+        ]
+    )
+
+    img_tensor = torch.tensor(img, dtype=torch.float32).cuda().unsqueeze(0)
+    return resize_transform(img_tensor) / 255.0
